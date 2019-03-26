b1adeb0ac03b50783ae7f17cc0ad4cfe77a5a8fa
WiZZy <bilucristian25@gmail.com>
2019-03-26 13:44:07 +0200

    Update the sql

--- sql/npc_codebox.sql
+++ sql/npc_codebox.sql
@@ -1,50 +1,12 @@
 -- ######################################################--
 --	NPC CODEBOX TABLES
 -- ######################################################--
+
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for lootcode_items
--- ----------------------------
-DROP TABLE IF EXISTS `lootcode_items`;
-CREATE TABLE `lootcode_items` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(32) DEFAULT NULL,
-  `itemId` int(7) unsigned DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `quantity` int(14) NOT NULL DEFAULT '1',
-  `gold` int(14) unsigned DEFAULT NULL,
-  `charges` tinyint(5) DEFAULT '1',
-  `isUnique` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of lootcode_items
--- ----------------------------
-INSERT INTO `lootcode_items` VALUES ('1', 'sixbags', '21841', 'Netherweave Bag', '6', '0', '2', '0');
-INSERT INTO `lootcode_items` VALUES ('2', 'artifact', '4696', 'Lapidis Tankard of Tidesippe', '1', '5', '3', '0');
-INSERT INTO `lootcode_items` VALUES ('3', 'ballroom', '3419', 'Red Rose', '1', '100', '3', '0');
-INSERT INTO `lootcode_items` VALUES ('4', 'ballroom', '6833', 'Tuxedo Shirt', '1', '0', '3', '0');
-INSERT INTO `lootcode_items` VALUES ('5', 'ballroom', '6835', 'Tuxedo Pants', '1', '0', '3', '0');
-
--- ----------------------------
--- Table structure for lootcode_player
--- ----------------------------
-DROP TABLE IF EXISTS `lootcode_player`;
-CREATE TABLE `lootcode_player` (
-  `id` int(7) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) DEFAULT NULL,
-  `playerGUID` int(7) unsigned DEFAULT NULL,
-  `isUnique` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `redeemed` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- ######################################################--
+-- --------------------------------------------------------------------------------------
 --	CODEBOX - 601021
--- ######################################################--
+-- --------------------------------------------------------------------------------------
 SET
 @Entry 		:= 601021,
 @Model 		:= 16804, -- Elven Jeweler
@@ -72,3 +34,61 @@ INSERT INTO creature_template (entry, modelid1, name, subname, IconName, gossip_
 -- NPC Text
 DELETE FROM `npc_text` WHERE `ID`=@Entry;
 INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry, 'Greetings $N. Do you have a loot code to redeem?');
+DELETE FROM `npc_text` WHERE `ID`=@Entry+1;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry+1, '[GM] Add Loot Code');
+DELETE FROM `npc_text` WHERE `ID`=@Entry+18;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry+18, '[GM] Delete Loot Code');
+DELETE FROM `npc_text` WHERE `ID`=@Entry+28;
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry+28, '[GM] View Loot Codes');
+
+-- --------------------------------------------------------------------------------------
+-- Table structure for lootcode_items
+-- --------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS `lootcode_items`;
+CREATE TABLE `lootcode_items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(32) DEFAULT NULL,
+  `itemId` int(7) unsigned DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `quantity` int(14) NOT NULL DEFAULT '1',
+  `gold` int(14) unsigned DEFAULT NULL,
+  `customize` int(14) unsigned DEFAULT '0',
+  `charges` tinyint(5) DEFAULT '1',
+  `isUnique` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------------------------------------
+-- Default Records of lootcode_items
+-- Q 	= Quantity
+-- G 	= Gold
+-- CU	= Customize
+-- C	= Charges
+-- U	= Is Unique Item
+-- -------------------------------------------------------------------------------------------------------------------
+--                                    ID     CODE        ITEMID   NAME             Q     G   CU    C    U
+-- -------------------------------------------------------------------------------------------------------------------
+INSERT INTO `lootcode_items` VALUES ('1', 'threebags', '21841', 'Netherweave Bag', '3', '0', '0', '1', '0');
+INSERT INTO `lootcode_items` VALUES ('2', 'artifact', '4696', 'Lapidis Tankard of Tidesippe', '1', '0', '0','1', '1');
+INSERT INTO `lootcode_items` VALUES ('3', 'ballroom', '3419', 'Red Rose', '1', '0', '0', '3', '0');
+INSERT INTO `lootcode_items` VALUES ('4', 'ballroom', '6833', 'Tuxedo Shirt', '1', '0', '0', '3', '0');
+INSERT INTO `lootcode_items` VALUES ('5', 'ballroom', '6835', 'Tuxedo Pants', '1', '0', '0', '3', '0');
+INSERT INTO `lootcode_items` VALUES ('6', 'lockpick', '15869', 'Skeleton Key', '5', '0', '0', '3', '0');
+INSERT INTO `lootcode_items` VALUES ('7', 'faction', '0', 'Faction Change', '1', '0', '1', '1', '0');
+INSERT INTO `lootcode_items` VALUES ('8', 'race', '0', 'Race Change', '1', '0', '2', '1', '0');
+INSERT INTO `lootcode_items` VALUES ('9', 'rename', '0', 'Rename Character', '1', '0', '3', '1', '0');
+
+-- --------------------------------------------------------------------------------------
+-- Table structure for lootcode_player
+-- Comment this to avoid overwriting existing 'lootcore_player' data.
+-- --------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS `lootcode_player`;
+CREATE TABLE `lootcode_player` (
+  `id` int(7) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `playerGUID` int(7) unsigned DEFAULT NULL,
+  `playerName` varchar(100) DEFAULT NULL,
+  `isUnique` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `redeemed` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
