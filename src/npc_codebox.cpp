e416a01b375c3f9deb5867856936fce6648d9673
Micrah <mic_24@live.com>
2019-04-06 13:05:26 +1100

    fix global announcer, tested and working deb 9

--- src/npc_codebox.cpp
+++ src/npc_codebox.cpp
@@ -78,7 +78,32 @@ player.
 
 */
 
-#include "Config.h"
+#include "ScriptMgr.h"
+
+bool CBAnnounceModule;
+
+class CodeboxConfig : public WorldScript
+{
+public:
+	CodeboxConfig() : WorldScript("CodeboxConfig_conf") { }
+
+	void OnBeforeConfigLoad(bool reload) override
+	{
+		if (!reload) {
+			std::string conf_path = _CONF_DIR;
+			std::string cfg_file = conf_path + "/npc_codebox.conf";
+#ifdef WIN32
+			cfg_file = "npc_codebox.conf";
+#endif
+			std::string cfg_def_file = cfg_file + ".dist";
+			sConfigMgr->LoadMore(cfg_def_file.c_str());
+			sConfigMgr->LoadMore(cfg_file.c_str());
+
+			CBAnnounceModule = sConfigMgr->GetBoolDefault("CodeboxNPC.Announce", 1);
+						
+		}
+	}
+};
 
 class CodeboxAnnounce : public PlayerScript
 {
@@ -87,13 +112,11 @@ public:
 
     CodeboxAnnounce() : PlayerScript("CodeboxAnnounce") {}
 
-    void OnLogin(Player* player)
+    void OnLogin(Player* player)  override
     {
-        // Announce Module
-        if (sConfigMgr->GetBoolDefault("CodeboxNPC.Announce", true))
-        {
-            ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00CodeboxNPC |rmodule.");
-        }
+       	if (sConfigMgr->GetBoolDefault("CodeboxNPC.Announce", true)) {
+			ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00CodeboxNPC |rmodule.");
+		}
     }
 };
 
